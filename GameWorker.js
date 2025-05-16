95bab74f3612134f94957289742c3f89b63227d8
Georgi Georgiev <georgi.toni.georgiev@gmail.com>
2025-05-16 12:15:11 +0300

    feat: created worker file

---
+++ GameWorker.js
@@ -0,0 +1,24 @@
+let lastTick;
+let tickLength;
+
+function initialize({ lastTick: lt, tickLength: tl }) {
+    lastTick = lt;
+    tickLength = tl;
+}
+
+self.onmessage = function (e) {
+    const { type, payload } = e.data;
+
+    if (type === 'init') {
+        initialize(payload);
+    } else if (type === 'update') {
+        let { numTicks } = payload;
+        let updates = [];
+        for (let i = 0; i < numTicks; i++) {
+            lastTick += tickLength;
+            // TODO: Replace this with your actual game update logic
+            updates.push({ tick: lastTick });
+        }
+        self.postMessage({ type: 'updated', payload: { lastTick, updates } });
+    }
+};
